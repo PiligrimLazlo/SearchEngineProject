b25b905f0c49ce8a40fee1e98f75f569fd87b1c5
polushkin.vasiliy@yandex.ru <Passwordforgitlab.2022>
2022-07-13 00:39:15 +0300

    hibernate ant db connection initial added but doesn't work

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static Connection connection;
+
+    private static final String dbName = "search_engine";
+    private static final String dbUser = "root";
+    private static final String dbPass = "PasswordforMySQL.2022";
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/" + dbName , dbUser, dbPass);
+                connection.createStatement().execute("DROP TABLE IF EXISTS page");
+                connection.createStatement().execute("CREATE TABLE page(" +
+                        "id INT NOT NULL AUTO_INCREMENT, " +
+                        "path TEXT NOT NULL, " +
+                        "code INT NOT NULL, " +
+                        "content MEDIUMTEXT NOT NULL, " +
+                        "PRIMARY KEY(id))");
+                connection.createStatement().execute("CREATE INDEX page_index ON page (path(50));");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/Page.java
@@ -0,0 +1,44 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "page")
+public class Page {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String path;
+    private int code;
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
